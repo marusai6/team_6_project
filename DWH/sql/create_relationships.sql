d851ff68196cdfa1e4a8eb280b940ff340fa2937
Karina <avakkyan@gmail.com>
2024-07-11 17:42:46 +0300

    feat: update sql skripts

--- DWH/sql/create_relationships.sql
+++ DWH/sql/create_relationships.sql
@@ -1,145 +1,318 @@
--- Скрипт для создания отношений между таблицами
+CREATE TABLE базы_данных
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.базы_данных_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_база_данных
-FOREIGN KEY (база_данных_id) REFERENCES dds.базы_данных(id);
+CREATE TABLE базы_данных_и_уровень_знаний_сотрудников
+(
+  id                SERIAL      ,
+  название          VARCHAR(255) NOT NULL,
+  активность        BOOLEAN     ,
+  дата_изм          TIMESTAMP   ,
+  база_данных_id    SERIAL       NOT NULL,
+  уровень_знаний_id INT         ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.базы_данных_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_уровень_знаний_бд
-FOREIGN KEY (уровень_знаний_id) REFERENCES dds.уровни_знаний(id);
+CREATE TABLE инструменты
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  id         SERIAL       NOT NULL,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.инструменты_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_инструменты
-FOREIGN KEY (инструменты_id) REFERENCES dds.инструменты(id);
+CREATE TABLE инструменты_и_уровень_знаний_сотрудников
+(
+  id                SERIAL      ,
+  user_id           INT         ,
+  название          VARCHAR(255) NOT NULL,
+  активность        BOOLEAN     ,
+  дата_изм          TIMESTAMP   ,
+  дата              DATE        ,
+  инструменты_id    INT         ,
+  уровень_знаний_id INT         ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.инструменты_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_уровень_знаний_инструменты
-FOREIGN KEY (уровень_знаний_id) REFERENCES dds.уровни_знаний(id);
+CREATE TABLE образование_пользователей
+(
+  id                          SERIAL      ,
+  user_id                     INT          NOT NULL,
+  активность                  BOOLEAN     ,
+  дата_изм                    TIMESTAMP   ,
+  уровень_образования         VARCHAR(255),
+  название_учебного_заведения VARCHAR(255),
+  год_окончания               INT         ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.образование_пользователей
-ADD CONSTRAINT fk_образование_пользователи
-FOREIGN KEY (user_id) REFERENCES dds.сотрудники_дар(id);
+CREATE TABLE опыт_сотрудника_в_отраслях
+(
+  id                          SERIAL   ,
+  user_id                     INT       NOT NULL,
+  активность                  BOOLEAN  ,
+  дата                        TIMESTAMP,
+  отрасли_id                  INT      ,
+  уровень_знаний_в_отрасли_id INT      ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.опыт_сотрудника_в_отраслях
-ADD CONSTRAINT fk_отрасли
-FOREIGN KEY (отрасли_id) REFERENCES dds.отрасли(id);
+CREATE TABLE опыт_сотрудника_в_предметных_областях
+(
+  id                                     SERIAL   ,
+  user_id                                INT       NOT NULL,
+  активность                             BOOLEAN  ,
+  дата                                   TIMESTAMP,
+  предметная_область_id                  INT      ,
+  уровень_знаний_в_предметной_области_id INT      ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.опыт_сотрудника_в_отраслях
-ADD CONSTRAINT fk_уровень_знаний_в_отрасли
-FOREIGN KEY (уровень_знаний_в_отрасли_id) REFERENCES dds.уровни_знаний(id);
+CREATE TABLE отрасли
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.опыт_сотрудника_в_предметных_областях
-ADD CONSTRAINT fk_предметная_область
-FOREIGN KEY (предметная_область_id) REFERENCES dds.предметная_область(id);
+CREATE TABLE платформы
+(
+  id         Serial       NOT NULL,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.опыт_сотрудника_в_предметных_областях
-ADD CONSTRAINT fk_уровень_знаний_в_предметной_области
-FOREIGN KEY (уровень_знаний_в_предметной_области_id) REFERENCES dds.уровни_знаний(id);
+CREATE TABLE платформы_и_уровень_знаний_сотруд
+(
+  id             SERIAL NOT NULL,
+  user_id        SERIAL NOT NULL,
+  активность     bool  ,
+  Дата изм.      DATE  ,
+  дата           DATE  ,
+  платформы      Serial NOT NULL,
+  уровень знаний SERIAL NOT NULL,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_user_id
-FOREIGN KEY (user_id) REFERENCES dds.сотрудники_дар(id);
+CREATE TABLE предметная_область
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_образование
-FOREIGN KEY (образование) REFERENCES dds.образование_пользователей(id);
+CREATE TABLE сертификаты_пользователей
+(
+  id                              SERIAL      ,
+  user_id                         INT          NOT NULL,
+  активность                      BOOLEAN     ,
+  дата_изм                        TIMESTAMP   ,
+  год_сертификата                 INT         ,
+  наименование_сертификата        VARCHAR(255),
+  организация_выдавшая_сертификат VARCHAR(255),
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_сертификаты_курсы
-FOREIGN KEY (сертификаты_курсы) REFERENCES dds.сертификаты_пользователей(id);
+CREATE TABLE сотрудники_дар
+(
+  id                    SERIAL      ,
+  дата_рождения         DATE        ,
+  активность            BOOLEAN     ,
+  пол                   VARCHAR(10) ,
+  фамилия               VARCHAR(255),
+  имя                   VARCHAR(255),
+  последняя_авторизация TIMESTAMP   ,
+  должность             VARCHAR(255),
+  цфо                   VARCHAR(255),
+  дата_регистрации      DATE        ,
+  дата_изменения        TIMESTAMP   ,
+  подразделения         VARCHAR(255),
+  email                 VARCHAR(255),
+  логин                 VARCHAR(255),
+  компания              VARCHAR(255),
+  город_проживания      VARCHAR(255),
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_языки
-FOREIGN KEY (языки) REFERENCES dds.языки(id);
+CREATE TABLE среды_разработки
+(
+  id            SERIAL      ,
+  название      VARCHAR(255) NOT NULL,
+  активность    BOOLEAN     ,
+  дата_изм      TIMESTAMP   ,
+  инструмент_id INT         ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_базы_данных
-FOREIGN KEY (базы_данных) REFERENCES dds.базы_данных(id);
+CREATE TABLE среды_разработки_и_уровень_знаний_сотрудников
+(
+  id                  SERIAL   ,
+  user_id             INT       NOT NULL,
+  активность          BOOLEAN  ,
+  дата_изм            TIMESTAMP,
+  дата                DATE     ,
+  среды_разработки_id INT      ,
+  уровень_знаний_id   INT      ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_инструменты
-FOREIGN KEY (инструменты) REFERENCES dds.инструменты(id);
+CREATE TABLE технологии
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_отрасли
-FOREIGN KEY (отрасли) REFERENCES dds.отрасли(id);
+CREATE TABLE технологии_и_уровень_знаний_сотрудников
+(
+  id                SERIAL   ,
+  user_id           INT       NOT NULL,
+  активность        BOOLEAN  ,
+  дата_изм          TIMESTAMP,
+  технологии_id     INT      ,
+  уровень_знаний_id INT      ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_платформы
-FOREIGN KEY (платформы) REFERENCES dds.платформы(id);
+CREATE TABLE типы_систем
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_предметные_области
-FOREIGN KEY (предметные_области) REFERENCES dds.предметная_область(id);
+CREATE TABLE типы_систем_и_уровень_знаний_сотрудников
+(
+  id                SERIAL   ,
+  user_id           INT       NOT NULL,
+  активность        BOOLEAN  ,
+  дата_изм          TIMESTAMP,
+  типы_систем_id    INT      ,
+  уровень_знаний_id INT      ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_среда_разработки
-FOREIGN KEY (среда_разработки) REFERENCES dds.среды_разработки(id);
+CREATE TABLE уровень_образования
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата       TIMESTAMP   ,
+  id         SERIAL       NOT NULL,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_типы_систем
-FOREIGN KEY (типы_систем) REFERENCES dds.типы_систем(id);
+CREATE TABLE уровни_владения_ин
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_фреймворки
-FOREIGN KEY (фреймворки) REFERENCES dds.фреймворки(id);
+CREATE TABLE уровни_знаний
+(
+  id         SERIAL       NOT NULL,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_языки_программирования
-FOREIGN KEY (языки_программирования) REFERENCES dds.языки_программирования(id);
+CREATE TABLE уровни_знаний_в_отрасли
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.резюмедар
-ADD CONSTRAINT fk_технологии
-FOREIGN KEY (технологии) REFERENCES dds.технологии(id);
+CREATE TABLE уровни_знаний_в_предметной_области
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  сорт       INT         ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.сертификаты_пользователей
-ADD CONSTRAINT fk_сертификаты_пользователи
-FOREIGN KEY (user_id) REFERENCES dds.сотрудники_дар(id);
+CREATE TABLE фреймворки
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.среды_разработки_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_среды_разработки
-FOREIGN KEY (среды_разработки_id) REFERENCES dds.среды_разработки(id);
+CREATE TABLE фреймворки_и_уровень_знаний_сотрудников
+(
+  id                SERIAL   ,
+  user_id           INT       NOT NULL,
+  активность        BOOLEAN  ,
+  дата_изм          TIMESTAMP,
+  фреймворки_id     INT      ,
+  уровень_знаний_id INT      ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.среды_разработки_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_уровень_знаний_среды_разработки
-FOREIGN KEY (уровень_знаний_id) REFERENCES dds.уровни_знаний(id);
+CREATE TABLE языки
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.типы_систем_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_типы_систем
-FOREIGN KEY (типы_систем_id) REFERENCES dds.типы_систем(id);
+CREATE TABLE языки_пользователей
+(
+  id                                   SERIAL   ,
+  user_id                              INT       NOT NULL,
+  активность                           BOOLEAN  ,
+  дата_изм                             TIMESTAMP,
+  язык_id                              INT      ,
+  уровень_знаний_иностранного_языка_id INT      ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.типы_систем_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_уровень_знаний_типы_систем
-FOREIGN KEY (уровень_знаний_id) REFERENCES dds.уровни_знаний(id);
+CREATE TABLE языки_программирования
+(
+  id         SERIAL      ,
+  название   VARCHAR(255) NOT NULL,
+  активность BOOLEAN     ,
+  дата_изм   TIMESTAMP   ,
+  PRIMARY KEY (id)
+);
 
-ALTER TABLE dds.технологии_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_технологии
-FOREIGN KEY (технологии_id) REFERENCES dds.технологии(id);
-
-ALTER TABLE dds.технологии_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_уровень_знаний_технологии
-FOREIGN KEY (уровень_знаний_id) REFERENCES dds.уровни_знаний(id);
-
-ALTER TABLE dds.фреймворки_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_фреймворки
-FOREIGN KEY (фреймворки_id) REFERENCES dds.фреймворки(id);
-
-ALTER TABLE dds.фреймворки_и_уровень_знаний_сотрудников
-ADD CONSTRAINT fk_уровень_знаний_фреймворки
-FOREIGN KEY (уровень_знаний_id) REFERENCES dds.уровни_знаний(id);
-
-ALTER TABLE dds.языки_пользователей
-ADD CONSTRAINT fk_языки
-FOREIGN KEY (язык_id) REFERENCES dds.языки(id);
-
-ALTER TABLE dds.языки_пользователей
-ADD CONSTRAINT fk_уровень_знаний_языки
-FOREIGN KEY (уровень_знаний_иностранного_языка_id) REFERENCES dds.уровни_владения_ин(id);
-
-ALTER TABLE dds.языки_программирования_и_уровень_знаний
-ADD CONSTRAINT fk_языки_программирования
-FOREIGN KEY (язык_программирования_id) REFERENCES dds.языки_программирования(id);
-
-ALTER TABLE dds.языки_программирования_и_уровень_знаний
-ADD CONSTRAINT fk_уровень_знаний_языки_программирования
-FOREIGN KEY (уровень_знаний_id) REFERENCES dds.уровни_знаний(id);
+CREATE TABLE языки_программирования_и_уровень_знаний
+(
+  id                       SERIAL   ,
+  user_id                  INT       NOT NULL,
+  активность               BOOLEAN  ,
+  дата_изм                 TIMESTAMP,
+  дата                     DATE     ,
+  уровень_знаний_id        INT      ,
+  язык_программирования_id INT      ,
+  PRIMARY KEY (id)
+);
