3643eaacef42a55ecc3e2a378531eb2269b50cd2
Мария Позднякова <105544662+marusai6@users.noreply.github.com>
2024-08-01 13:20:23 +0300

    Update import_and_compute.py

--- DWH/dags/import_and_compute.py
+++ DWH/dags/import_and_compute.py
@@ -1,40 +1,38 @@
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime
-from airflow.models import Variable
-from airflow.operators.bash import BashOperator
+import Scripts.prepare_dm
+import Scripts.extract_data
+import Scripts.transform_data
 
-
-config = Variable.get("time_start", deserialize_json=True)
 
 with DAG(
-    'import_and_compute_data',
+    'import_and_compute_2',
     default_args={
         'owner': 'me',
         'start_date': datetime(2024, 7, 25),
+
     },
     schedule_interval='@monthly',
-    catchup=False
 ) as dag:
-
     start_step = EmptyOperator(task_id="start_step", dag=dag)
 
-    task_1 = BashOperator(
-        task_id="import_source_data_to_ods",
-        bash_command='C:/Users/Acer/Desktop/Study/Stag/Stag_project/airflow/dags/scripts/extract_data.py',
-)
+    import_source_data_to_ods = PythonOperator(
+        task_id='import_source_data',
+        python_callable=Scripts.extract_data.main,
+    )
 
-    task_2= BashOperator(
-        task_id="prepare_dds",
-        bash_command='C:/Users/Acer/Desktop/Study/Stag/Stag_project/airflow/dags/scripts/prepare_dds.py',
-)
+    prepare_dds = PythonOperator(
+        task_id='prepare_dds',
+        python_callable=Scripts.transform_data.main,
+    )
 
-    task_3 = BashOperator(
-        task_id="prepare_dm",
-        bash_command='C:/Users/Acer/Desktop/Study/Stag/Stag_project/airflow/dags/scripts/prepare_datamart_test.py',
-
-)
+    prepare_dm = PythonOperator(
+        task_id='prepare_dm',
+        python_callable=Scripts.prepare_dm.main,
+    )
 
     end_step = EmptyOperator(task_id="end_step", dag=dag)
 
-    start_step >> task_1 >> task_2 >> task_3 >> end_step
+    start_step >> import_source_data_to_ods >> prepare_dds >> prepare_dm >> end_step
