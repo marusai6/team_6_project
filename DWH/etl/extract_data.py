0e1f4ef1c022691320aed2edaef37f49be2330c2
Мария Позднякова <105544662+marusai6@users.noreply.github.com>
2024-08-02 16:52:52 +0300

    Update extract_data.py под airflow

--- DWH/etl/extract_data.py
+++ DWH/etl/extract_data.py
@@ -1,42 +1,45 @@
-import pandas as pd
-import configparser
-from sqlalchemy import create_engine
+def main():
+    import pandas as pd
+    import configparser
+    from sqlalchemy import create_engine
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+    config = configparser.ConfigParser()
+    config.read('/opt/airflow/dags/Scripts/config.ini')
 
-source_host = config['source_database']['host']
-source_port = config['source_database']['port']
-source_database = config['source_database']['database']
-source_user = config['source_database']['user']
-source_password = config['source_database']['password']
+    source_host = config['source_database']['db_host']
+    source_port = config['source_database']['db_port']
+    source_database = config['source_database']['db_name']
+    source_user = config['source_database']['db_user']
+    source_password = config['source_database']['db_password']
 
-target_host = config['target_database']['host']
-target_port = config['target_database']['port']
-target_database = config['target_database']['database']
-target_user = config['target_database']['user']
-target_password = config['target_database']['password']
+    db_host = config['Database']['db_host']
+    db_port = config['Database']['db_port']
+    db_name = config['Database']['db_name']
+    db_user = config['Database']['db_user']
+    db_password = config['Database']['db_password']
 
-source_conn_str = f"postgresql://{source_user}:{source_password}@{source_host}:{source_port}/{source_database}"
-target_conn_str = f"postgresql://{target_user}:{target_password}@{target_host}:{target_port}/{target_database}"
+    source_conn_str = f"postgresql://{source_user}:{source_password}@{source_host}:{source_port}/{source_database}"
+    target_conn_str = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
-source_engine = create_engine(source_conn_str)
-target_engine = create_engine(target_conn_str)
+    source_engine = create_engine(source_conn_str)
+    target_engine = create_engine(target_conn_str)
 
-def copy_table(table_name):
-    df = pd.read_sql(f'SELECT * FROM source_data.{table_name}', source_engine)
-    df.to_sql(table_name, target_engine, schema='ods', if_exists='replace', index=False)
-    print(f"Table {table_name} copied successfully")
+    def copy_table(table_name):
+        df = pd.read_sql(f'SELECT * FROM source_data.{table_name}', source_engine)
+        df.to_sql(table_name, target_engine, schema='ods', if_exists='replace', index=False)
+        print(f"Table {table_name} copied successfully")
 
-tables_query = """
+    tables_query = """
 SELECT table_name 
 FROM information_schema.tables
 WHERE table_schema = 'source_data'
 """
-tables = pd.read_sql(tables_query, source_engine)
+    tables = pd.read_sql(tables_query, source_engine)
 
-for table in tables['table_name']:
-    copy_table(table)
+    for table in tables['table_name']:
+        copy_table(table)
 
-print("All tables copied successfully")
+    print("All tables copied successfully")
 
+if __name__ == '__main__':
+    main()
